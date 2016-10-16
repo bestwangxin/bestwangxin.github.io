7d9264fa80844c8f908822db839391c478a51113
bestwangxin <794822947@qq.com>
2016-10-16 18:18:44 +0800

    Revert "blog"
    This reverts commit ba3ce14c2d91501312b362b109edaaffaa1310c7.

--- js/offcanvas.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ¹Å¹ÖµÄÌÇ¹û on 2016/10/14.
- */
-$(document).ready(function () {
-    $('[data-toggle="offcanvas"]').click(function () {
-        $('.row-offcanvas').toggleClass('active')
-    });
-});
